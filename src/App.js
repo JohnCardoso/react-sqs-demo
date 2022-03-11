f0ae5dd3b150faa92fa0a3ffc7b7904bcafb7af9
jonathan <jonathan.cardoso@celk.net>
2022-03-11 10:44:38 -0300

    Create demo sqs-react-client-control

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  SQSClient,
+  AddPermissionCommand,
+  ReceiveMessageCommand,
+} from '@aws-sdk/client-sqs';
+import { useEffect } from 'react';
 
 function App() {
+  const client = new SQSClient({
+    region: 'SET_YOUR_REGION',
+    credentials: {
+      accessKeyId: 'SET_YOUR_IAM_ACCESS_KEY',
+      secretAccessKey: 'SET_YOUR_IAM_SECRET_ACCESS_KEY',
+    },
+  });
+
+  const params = {
+    QueueUrl: 'YOUR_SQS_URL',
+    Label: 'NAME_YOUR_LABEL',
+    AWSAccountIds: ['SET_ACCOUNT_ID'],
+    //Define the actions that you want to use, to the maximum of 7 actions
+    Actions: ['ReceiveMessage'],
+  };
+
+  const params2 = {
+    QueueUrl: 'YOUR_SQS_URL',
+  };
+
+  const receiveCommand = new ReceiveMessageCommand(params2);
+
+  const command = new AddPermissionCommand(params);
+
+  useEffect(() => {
+    async function sqs() {
+      //display and delete the messages in the SQS console
+      try {
+        const data = await client.send(command);
+        const data2 = await client.send(receiveCommand);
+
+        console.log(data);
+        if (data2) {
+          await client.send(
+            new DeleteMessageCommand({
+              QueueUrl: params2.QueueUrl,
+              ReceiptHandle: data2.Messages[0].ReceiptHandle,
+            }),
+          );
+        }
+      } catch (error) {
+        alert(error.message);
+      } finally {
+        console.log('terminou');
+      }
+    }
+
+    sqs();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
